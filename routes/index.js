d9b915a3e8dbd6fc9bc4223a8d2a46be0bb27c4d
IanDs0 <ianhalominecraft@gmail.com>
2022-02-08 18:55:36 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+const { render } = require('express/lib/response');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index.html');
+  //res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
