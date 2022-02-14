559d1244b063ee8b2561e2cbd98e3942ab85c242
IanDs0 <ianhalominecraft@gmail.com>
2022-02-14 09:07:02 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html');
-  //res.render('index', { title: 'Express' });
+  //res.render('index.pug', { title: 'Express' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const { render } = require('express/lib/response');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index.pug',{title: 'Olá mundo'});
 });
 
 module.exports = router;
